0f44fd478021b4b32065aa3dc39a0afe7651e1a2
Illia <ilyaivasyk1@gmail.com>
2023-06-28 18:06:24 +0300

    lesson-5- Простое-общение-с-пользователем

---
+++ lesson-5-Простое-общение-с-пользователем/index.js
@@ -0,0 +1,18 @@
+// const result = confirm("Are tou here?");
+
+// console.log(result);
+
+// const answer = prompt("Are you 18?", true);
+// console.log(typeof(answer));
+
+// вся інформація від користовача є string
+
+// const answers = [];
+
+// answers[0] = prompt("what your name?");
+// answers[1] = prompt("what is your last name?");
+// answers[2] = prompt("How old are you?");
+
+// console.log(typeof(answers));
+
+console.log(typeof(null));//return object not null
