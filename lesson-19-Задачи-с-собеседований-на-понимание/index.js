ceea127e2b4848a8a461f7607b81dd49f5510dd8
Illya <ilyaivasyk1@gmail.com>
2023-07-02 14:08:25 +0300

    lesson-19-Задачи-с-собеседований-на-понимание

---
+++ lesson-19-Задачи-с-собеседований-на-понимание/index.js
@@ -0,0 +1,34 @@
+// let x = 5; 
+// alert(x++); //5
+
+// console.log([] + false - null + true);//NaN
+
+// let y = 1; 
+// let x = y = 2; alert(x); //2
+
+// console.log(typeof([] + 1 + 2)); //string 12
+
+// alert("1"[0]); //1
+// console.log(typeof ("1" [0])); //1
+
+// console.log(2 && 1 && null && 0 && undefined); //null
+// 
+
+//  console.log(!!(1 && 2) === (1 && 2));
+//not same
+//!! = true is not 1 && 2 it`s number
+
+// alert(null || 2 && 3 || 4); //3
+// || stop in true
+
+
+// const a = [1, 2, 3],
+// b = [1, 2, 3];
+//it`s not the same
+
+// alert(+"Infinity"); //Infinity
+
+// console.log("Ёжик" > "яблоко"); // false
+
+// console.log(0 || "" || 2 || undefined || true || false);
+//2
