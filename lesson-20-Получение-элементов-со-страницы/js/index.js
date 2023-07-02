82bd498e7d113bca62efe4c02f2cd1083c0b2032
Illya <ilyaivasyk1@gmail.com>
2023-07-02 14:08:57 +0300

    lesson-20-Получение-элементов-со-страницы

---
+++ lesson-20-Получение-элементов-со-страницы/js/index.js
@@ -0,0 +1,21 @@
+// const box = document.getElementById('box');
+// console.log(box);
+
+// const btns = document.getElementsByTagName('button');
+
+// console.log(btns[1]);
+
+// const circles = document.getElementsByClassName('circle');
+// console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+// console.log(hearts);
+
+hearts.forEach(items => {
+  console.log(items);
+}); 
+
+const oneHeart = document.querySelector('.heart');
+
+console.log(oneHeart);
